8cea1e034f3ba1bac19c6a5a8a51ee94004b8bde
alexphieler <alexander.phieler@elbflorace.de>
2024-03-24 10:10:05 +0100

    Add example launch file

---
+++ launch/example.launch.py
@@ -0,0 +1,44 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+
+def getFullFilePath(name, dir):
+    ret = os.path.join(
+        get_package_share_directory('pacsim'),
+        dir,
+        name
+    )
+    return ret
+
+
+def generate_launch_description():
+  track_name = "FSE23.yaml"
+  track_frame = "map"
+  realtime_ratio = 1.0
+  discipline = "autocross"
+
+  xacro_file_name = 'separate_model.xacro'
+  xacro_path = getFullFilePath(xacro_file_name, "urdf")
+
+
+  nodePacsim = Node(
+          package='pacsim',
+          namespace='pacsim',
+          executable='pacsim_node',
+          name='pacsim_node',
+          parameters = [{'use_sim_time':True}, {"track_name" : getFullFilePath(track_name, "tracks")}, {"track_frame" : track_frame}, {"realtime_ratio" : realtime_ratio}, {"report_file_dir" : "/tmp"}, {"perception_config_path" : getFullFilePath("perception.yaml", dir="config")}, {"sensors_config_path" : getFullFilePath("sensors.yaml", dir="config")}, {"vehicle_model_config_path" : getFullFilePath("vehicleModel.yaml", dir="config")}, {"discipline" : discipline}],
+          output="screen",
+          emulate_tty=True)
+  robot_state_publisher= Node(
+          package='robot_state_publisher',
+          executable='robot_state_publisher',
+          name='robot_state_publisher',
+          output='screen',
+          parameters=[{'use_sim_time':True}, {'publish_frequency':float(1000),
+                      'robot_description': Command(['xacro',' ',xacro_path])
+                      }],
+          arguments=[xacro_path])
+  return LaunchDescription([nodePacsim, robot_state_publisher])
