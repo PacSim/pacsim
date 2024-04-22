bc080dab69606f549796635bc1443049ce818491
lourenco31 <r.lourenco.a@gmail.com>
2024-04-22 16:10:31 +0100

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ rosdep install --from-paths src --ignore-src -y &&
 colcon build && 
 source ./install/setup.bash && 
 ros2 launch pacsim example.launch.py & 
+cd /home/ws/ &&
 source ./install/setup.bash && 
 ros2 launch foxglove_bridge foxglove_bridge_launch.xml # Foxglove bridge in parallel
