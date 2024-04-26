474d6cc53fc3cd1060f0cf6bebdeb0f439706199
marhcouto <marhcouto1101@gmail.com>
2024-04-26 12:49:17 +0000

    fix: fixed entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
+echo "HOSTNAME OUTPUT:"
+hostname -I
+
+echo "\n"
+
 sudo apt update && 
 rosdep update && 
 rosdep install --from-paths src --ignore-src -y && 
-colcon build && 
+colcon build &
+
+wait $!
+
+cd /home/ws/ &&
 source ./install/setup.bash && 
 ros2 launch pacsim example.launch.py & 
+
 cd /home/ws/ &&
 source ./install/setup.bash && 
 ros2 launch foxglove_bridge foxglove_bridge_launch.xml # Foxglove bridge in parallel
