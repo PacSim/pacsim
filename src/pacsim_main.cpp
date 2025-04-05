1fd947e080d873933ea4e377b00c7400850adabc
alexphieler <alexander.phieler@elbflorace.de>
2025-04-05 20:25:23 +0000

    Forgot include

--- src/pacsim_main.cpp
+++ src/pacsim_main.cpp
@@ -17,6 +17,7 @@
 #include <tf2_ros/transform_broadcaster.h>
 
 #include "VehicleModel/VehicleModelBicycle.cpp"
+#include "VehicleModel/VehicleModel4Wheel.cpp"
 #include "configParser.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
 #include "visualization_msgs/msg/marker_array.hpp"
