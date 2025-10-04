040b4b90d3cf91ed657a56bf882544b4aeb87bbd
alexphieler <alexander.phieler@elbflorace.de>
2025-10-04 17:18:25 +0000

    Small fix

--- src/ros2Helpers.cpp
+++ src/ros2Helpers.cpp
@@ -463,7 +463,7 @@ pacsim::msg::Track createRosTrackMessage(const Track& data, std::string frameId,
         ret.path_left_point_indices.push_back(val);
     }
     for (auto val : data.path_right_point_indices) {
-        ret.path_right_point_indices.push_back(val);
+        ret.path_right_points_indices.push_back(val);
     }
 
     return ret;
